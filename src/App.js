a8273cd7681538cb143f03a133dc643bc64d6309
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-06-25 19:08:03 -0300

    adicao de colaboradores na lista

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
 
-  const [colaboradores, setColaboradores] = useState([])
-  
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
-    setColaboradores([...colaboradores, colaborador])
-  }
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
 
   return (
     <div className="App">
       <Banner />
-      <Formulario 
-        aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} 
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
       />
     </div>
   );
