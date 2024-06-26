2958f655888ffaa7fa5f3f89e40a48b733d3fd10
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-06-26 11:39:42 -0300

    clonando

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,39 +1,36 @@
-import './Rodape.css'
 
-import React from 'react'
+import './rodape.css'
 
 const Rodape = () => {
-  return (
-    <footer className='footer'>
-      <section>
-        <ul>
-          <li>
-            <a href='facebook.com' target='_blank'>
-              <img src='/imagens/fb.png' alt='facebook' />
-            </a>
-          </li>
-          <li>
-            <a href='twitter.com' target='_blank'>
-              <img src='/imagens/tw.png' alt='twitter' />
-            </a>
-          </li>
-          <li>
-            <a href='instagram.com' target='_blank'>
-              <img src='/imagens/ig.png' alt='instagram' />
-            </a>
-          </li>
+    return (<footer className="footer">
+       <section>
+       <ul>
+            <li>
+                <a href="facebook.com" target="_blank">
+                    <img src="/imagens/facebook.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="twitter.com" target="_blank">
+                    <img src="/imagens/twitter.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="instagram.com" target="_blank">
+                    <img src="/imagens/instagram.png" alt="" />
+                </a>
+            </li>
         </ul>
-      </section>
-      <section>
-        <img src='/imagens/logo.png' alt='logo' />
-      </section>
-      <section>
+       </section>
+       <section>
+       <img src="/imagens/logo.png" alt="" />
+       </section>
+       <section>
         <p>
-          W3Soft© 2024 - Todos os direitos reservados
+            Desenvolvido por Alura.
         </p>
-      </section>
-    </footer>
-  )
+       </section>
+    </footer>)
 }
 
 export default Rodape
