9754820efcb6d6509b7a646db437532c24c2c548
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-06-25 22:40:35 -0300

    refatorando formulario

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -5,15 +5,7 @@ import Botao from "../Botao";
 import { useState } from "react";
 
 const Formulario = (props) => {
-  const times = [
-    "Programação",
-    "Front-end",
-    "Data Science",
-    "Devops",
-    "Ux e Design",
-    "Mobile",
-    "Inovação e Gestão",
-  ];
+ 
 
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
@@ -56,7 +48,7 @@ const Formulario = (props) => {
           <ListaSuspensa 
             obrigatorio={true} 
             label="Time" 
-            itens={times} 
+            itens={props.times} 
             valor={time}
             aoAlterado={valor => setTime(valor)}
           />
